32eafd4f19e34e6e8b08b1e9ff6c09b36f0ee31f
JaimeRomero <jaime_im@outlook.com>
2022-05-12 17:35:52 -0500

    Se valido el funcionamiento del metodo getAmountOfExplorersByMission desde la consola

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Reader = require("./lib/utils/Reader")
 const ExplorerService = require("./lib/services/ExplorerService")
 const explorers = Reader.readJsonFile("explorers.json")
-console.log(ExplorerService.filterByMission(explorers, "node"))
+//console.log(ExplorerService.filterByMission(explorers, "node"))
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
