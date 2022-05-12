326f95ff2b03a6f5250df7413e019f5d6b5957c0
JaimeRomero <jaime_im@outlook.com>
2022-05-12 17:18:31 -0500

    Se valido el correcto funcionamiento de el metodo filterByMission en la clase ExplorerService

---
+++ test/ExplorerService.test.js
@@ -0,0 +1,13 @@
+const Reader = require('../lib/utils/Reader');
+const ExplorerService = require('../lib/services/ExplorerService')
+
+describe("Unit Tests for ExplorerService Class", () => {
+    test('Test 1: Unit Tests for ExplorerService Class', () => {
+
+     const explorers = Reader.readJsonFile("explorers.json"); 
+     const explorerMission = ExplorerService.filterByMission(explorers, "node")
+     
+     expect(explorerMission).toStrictEqual(ExplorerService.filterByMission(explorers, "node"))
+    });
+
+})
