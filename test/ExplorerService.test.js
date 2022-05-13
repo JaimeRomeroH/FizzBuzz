f01b2e5863cf7b628476ffa3dbfd8409b64299f6
JaimeRomero <jaime_im@outlook.com>
2022-05-13 01:46:45 -0500

    Se aplico eslinter a todos los archivos

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -1,28 +1,28 @@
-const Reader = require('../lib/utils/Reader');
-const ExplorerService = require('../lib/services/ExplorerService')
+const Reader = require("../lib/utils/Reader");
+const ExplorerService = require("../lib/services/ExplorerService");
 
 describe("Unit Tests for ExplorerService Class", () => {
-    test('Test 1: Unit Tests for ExplorerService Class', () => {
+    test("Test 1: Unit Tests for ExplorerService Class", () => {
 
-     const explorers = Reader.readJsonFile("explorers.json"); 
-     const explorerMission = ExplorerService.filterByMission(explorers, "node")
+        const explorers = Reader.readJsonFile("explorers.json"); 
+        const explorerMission = ExplorerService.filterByMission(explorers, "node");
      
-     expect(explorerMission).toStrictEqual(ExplorerService.filterByMission(explorers, "node"))
+        expect(explorerMission).toStrictEqual(ExplorerService.filterByMission(explorers, "node"));
     });
-     test('Test 2: Unit Tests for ExplorerService Class', () => {
+    test("Test 2: Unit Tests for ExplorerService Class", () => {
   
         const explorers = Reader.readJsonFile("explorers.json"); 
-        const explorerMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        const explorerMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
        
-        expect(explorerMission).toBe(10)
-       })
-     test('Test 3: Unit Tests for ExplorerService Class', () => {
+        expect(explorerMission).toBe(10);
+    });
+    test("Test 3: Unit Tests for ExplorerService Class", () => {
   
         const explorers = Reader.readJsonFile("explorers.json"); 
-        const explorerMission = ExplorerService.getExplorersUserNamesByMission(explorers, "node")
+        const explorerMission = ExplorerService.getExplorersUserNamesByMission(explorers, "node");
        
-        expect(explorerMission).toStrictEqual(ExplorerService.getExplorersUserNamesByMission(explorers, "node"))
-       })    
+        expect(explorerMission).toStrictEqual(ExplorerService.getExplorersUserNamesByMission(explorers, "node"));
+    });    
          
 
-})
+});
--- test/Reader.test.js
+++ test/Reader.test.js
@@ -1,12 +1,12 @@
-const Reader = require('../lib/utils/Reader');
+const Reader = require("../lib/utils/Reader");
 
 describe("Unit Tests for Reader Class", () => {
-    test('Test 1: Unit Tests for Reader Class', () => {
+    test("Test 1: Unit Tests for Reader Class", () => {
      
-      const file = 'explorers.json'
+        const file = "explorers.json";
       
-      const reading = Reader.readJsonFile(file) 
-      const data = require('./../explorers.json')
-      expect(reading).toStrictEqual(data)
+        const reading = Reader.readJsonFile(file); 
+        const data = require("./../explorers.json");
+        expect(reading).toStrictEqual(data);
     });
-  })
+});
