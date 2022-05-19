25a2b2596fd199a7598be94f44ba1501f4512cc0
JaimeRomero <jaime_im@outlook.com>
2022-05-18 20:05:43 -0500

    Se valido el funcionamiento de el metodo getExplorersByMission desde la consola

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService");
 const FizzbuzzService = require("./lib/services/FizzbuzzService");
+const ExplorerController = require("./lib/controllers/ExplorerController");
 const explorers = Reader.readJsonFile("explorers.json");
 //console.log(ExplorerService.filterByMission(explorers, "node"))
 //console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
 //console.log(ExplorerService.getExplorersUserNamesByMission(explorers, "node"))
-console.log(FizzbuzzService.applyValidationInExplorer(explorers, "node"));
+//console.log(FizzbuzzService.applyValidationInExplorer(explorers, "node"));
+console.log(ExplorerController.getExplorersByMission("node"))
